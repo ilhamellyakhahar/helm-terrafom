7b33cb3f40aaf96bbf6f29ac823cccb3766e8ba7
Ilham Ellya Khahar <ilham.ellya@nawatech.co>
2025-08-27 11:25:28 +0700

    added ingress controller

---
+++ nginx/ingress.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "ingress-nginx" {
+  name              = "ingress-nginx"
+  namespace         = "ingress"
+  create_namespace  = true
+
+  repository        = "https://kubernetes.github.io/ingress-nginx"
+  chart             = "ingress-nginx"
+
+  values = [file("values.yml")]
+}
